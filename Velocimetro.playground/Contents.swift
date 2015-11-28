083377e9e2997b488d81a05caba60973cb50f282
Gandhi Mena Salas <gandhimenasalas@macbook-pro-de-gandhi.local>
2015-11-28 17:18:07 -0600

    Falta que la iteración comience en Cero.

--- Velocimetro.playground/Contents.swift
+++ Velocimetro.playground/Contents.swift
@@ -25,7 +25,7 @@ enum Velocidades: Int{
 class Auto {
 
     var velocidad = Velocidades.self
-
+    
     init(velocidadInicial:Velocidades){
         
         self.velocidad.init(velocidadInicial:Velocidades.Apagado)
@@ -33,32 +33,89 @@ class Auto {
     }
     
     
-    func cambioDeVelocidad() -> (actual: Int, velocidadEnCadea:String){
-        
-        let actual = velocidad.Apagado.rawValue
-        let mensaje = " "
-        
-        let resultado = (actual, mensaje)
-        
-        return resultado
-        
-    }
+//    
+//    func cambioDeVelocidad() -> (actual: Int, velocidadEnCadea:String){
+//        
+//        let actual = velocidad.Apagado.rawValue
+//        let mensaje = " "
+//        
+//        let resultado = (actual, mensaje)
+//        
+//        return resultado
+//        
+//    }
     
     
+}
+
+var actual:Int = 0
+var velocidadEnCadena = ""
+
+
+func cambioDeVelocidades()-> (actual:Int, velocidadEnCadena:String){
+   
+
+    actual++
+    
+    
+    switch(actual){
+    
+    case 2:
+        
+        actual = 2
+    
+    case 2...19:
+        
+        actual = 19
+        
+    case 20:
+        
+        velocidadEnCadena = " Velocidad Baja "
+        actual = 20
+        
+    case 21...49:
+        
+        actual = 49
+        
+    case 50:
+        
+        velocidadEnCadena = "velocidad Media"
+        actual = 50
+        
+    case 51...119:
+        
+        actual = 119
+        
+    case 120:
+        
+        velocidadEnCadena = "velocidad Alta"
+    
+    case 121...155:
+        
+        actual = 49
+        
+        
+    default:
+        
+        velocidadEnCadena = " "
+
+        }
+
+    
+    return (actual, velocidadEnCadena)
+        
 }
 
 
 
 
+for (var cambios = 1; cambios <= 20; cambios++) {
+    
+    cambioDeVelocidades()
+    
+    print("\(cambios)" + " " + "\(cambioDeVelocidades())")
 
-
-
-
-//for (var cambios = 1; cambios <= 20; cambios++) {
-// 
-//   print("\(cambios)")
-//    
-//}
+}
 
 
 
